b1d2470eb247c9129f960a4657d9047c38679fc8
Sherif <muhammedsherif40@yahoo.com>
2022-09-08 12:35:07 -0700

    multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *
+ *  main - Prints String
+ *
+ *  Description: "\"Programming is like building a multilingual puzzle"
+ *
+ *  Return: always 0
+ *
+ **/
+
+int main(void)
+
+{
+      
+       puts("\"Programming is like building a multilingual puzzle");
+
+       return (0);
+}
+
+
