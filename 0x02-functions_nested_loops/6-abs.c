8d644b439e9c02f84fc5be2c8ac5e7a2667c360e
Sherif <muhammedsherif40@yahoo.com>
2022-09-13 12:08:40 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - computes absolute value of an integer
+ * @n: n is an integer
+ * Return: 0
+ */
+int _abs(int)
+{
+	if ( n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
