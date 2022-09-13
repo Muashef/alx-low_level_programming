cd333f7e932ef65b24d66e1f137208401cd6e705
Sherif <muhammedsherif40@yahoo.com>
2022-09-13 07:47:56 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet a-z
+ *  Return: void
+ */
+void print_alphabet(void)
+{
+	char m;
+
+	for (m = "a"; m <= "z"; m++)
+	{
+		_putchar(m);
+	}
+	_putchar("\n")
+}
