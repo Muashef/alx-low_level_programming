e41bcff423ac69eb25393926b8c4c3870a156499
Sherif <muhammedsherif40@yahoo.com>
2022-09-13 09:57:12 -0700

    ten alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char m, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (m = 'a'; m <= z m++)
+		{
+			_putchar(m);
+		}
+		_putchar('\n');
+	}
+}
