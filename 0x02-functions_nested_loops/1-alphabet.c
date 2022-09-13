4963a339310295261e9553fd6b910c11b0ee2701
Sherif <muhammedsherif40@yahoo.com>
2022-09-13 08:04:27 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet abc
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char m;
+
+	for (m = 'a'; m <= 'z'; m++)
+	{
+		_putchar(m);
+	}
+	_putchar('\n')
+}
