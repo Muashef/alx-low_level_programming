ec0b04a78467b60a6f66b146cd6d94d1175facb2
Sherif <muhammedsherif40@yahoo.com>
2022-09-16 02:13:30 -0700

    print diagnonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,13 +8,13 @@
 
 void print_diagonal(int n)
 {
-	int i, m;
+	int i, j;
 
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (m = 0; m < i; m++)
+			for (j = 0; j < i; j++)
 				_putchar(' ');
 			_putchar(92);
 			_putchar('\n');
