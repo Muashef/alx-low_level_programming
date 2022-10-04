fb8ff09b946f46b170b03269a7309fd975a42f29
Sherif <muhammedsherif40@yahoo.com>
2022-10-04 07:42:03 -0700

    print name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: code
+ * @argv: code
+ * Return: Always 0
+ */
+
+int __attribute__((unused)) main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
