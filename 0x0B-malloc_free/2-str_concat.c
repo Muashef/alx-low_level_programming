4f05df9dbd0d05a5f65191c50e379a3a3c01fb77
Sherif <muhammedsherif40@yahoo.com>
2022-10-05 01:55:12 -0700

    malloc, free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,7 +18,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; s1[j] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		;
 
 	for (j = 0; s2[j] != '\0'; j++)
@@ -36,7 +36,7 @@ char *str_concat(char *s1, char *s2)
 		strout[k] = s1[k];
 
 	limit = j;
-	for (j = 0; j <= limit; j++)
+	for (j = 0; j <= limit; k++, j++)
 		strout[k] = s2[j];
 
 	return (strout);
